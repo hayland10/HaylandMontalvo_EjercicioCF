a5ad37eb996d48c88f2ab419bed54c74ef67e4ee
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-10-11 08:10:16 -0500

    Agregar archivos de proyecto.

---
+++ Data/HaylandMontalvo_EjercicioCFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HaylandMontalvo_EjercicioCF.Models;
+
+namespace HaylandMontalvo_EjercicioCF.Data
+{
+    public class HaylandMontalvo_EjercicioCFContext : DbContext
+    {
+        public HaylandMontalvo_EjercicioCFContext (DbContextOptions<HaylandMontalvo_EjercicioCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HaylandMontalvo_EjercicioCF.Models.Burger> Burger { get; set; } = default!;
+    }
+}
